4861d8405d5d990cabbc25fb82ff9623c229c32a
Veronica <batraveronica07@gmail.com>
2026-08-10 17:54:24 +0530

    Number of islands

--- Data Structures & Algorithms/Graphs/number of islands.cpp
+++ Data Structures & Algorithms/Graphs/number of islands.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    vector<int> dx={0,0,1,-1};
+    vector<int> dy={1,-1,0,0};
     void dfs(int i,int j,vector<vector<bool>> &vis,vector<vector<char>> &grid){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0') return;
-        if(vis[i][j]) return;
+        if(i>=grid.size() || j>=grid[0].size() || i<0 || j<0 || vis[i][j]) return;
         vis[i][j]=true;
-        dfs(i+1,j,vis,grid);
-        dfs(i-1,j,vis,grid);
-        dfs(i,j+1,vis,grid);
-        dfs(i,j-1,vis,grid);
+        for(int k=0;k<4;k++){
+            int nx=i+dx[k];
+            int ny=j+dy[k];
+            if(nx>=0 && ny>=0 && nx<grid.size() && ny<grid[0].size() && grid[nx][ny]=='1' && !vis[nx][ny]) {
+                dfs(nx,ny,vis,grid);
+            }
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
         int ans=0;
@@ -16,7 +20,7 @@ public:
         vector<vector<bool>> vis(n,vector<bool>(m,false));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
+                if(grid[i][j]=='1' && !vis[i][j]) {
                     dfs(i,j,vis,grid);
                     ans++;
                 }
